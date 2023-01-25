8cf3fe6038c629e3e79760f58e037939e12d1cce
Alan Wheatley <mr.rubikscube@mail.ru>
2023-01-25 22:25:44 +0500

    Change to User_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_23_134337) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_25_165541) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -58,9 +58,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_134337) do
     t.string "content"
     t.date "begin"
     t.date "ending"
-    t.string "responsible"
     t.string "remark"
     t.string "status"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_licenses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
