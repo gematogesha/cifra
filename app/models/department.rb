6ffad8d0ef25ac914bb9e6153024e6c682ea9ff2
Alan Wheatley <mr.rubikscube@mail.ru>
2023-02-01 13:05:51 +0500

    Fix

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,6 @@
 class Department < ApplicationRecord
 
-    before_save :capitalize_title, :strip_ize, :upcase_title, :short_title
+    before_save :capitalize_title, :strip_ize, :upcase_title
 
     validates :number, numericality: {less_than_or_equal_to: 999}, uniqueness: true
     validates :title, presence: true, uniqueness: true
