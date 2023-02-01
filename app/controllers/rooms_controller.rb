6e5ba223065fa1cd46f324faa5cfeba0dccf00c9
Alan Wheatley <mr.rubikscube@mail.ru>
2023-02-01 13:05:25 +0500

    Add room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,20 @@
 class RoomsController < ApplicationController
+
+    def new
+        @new_room = Room.new
+        @rooms = Room.all
+    end
+
+    def create
+        @new_room = current_user&.rooms&.create
+
+        if @new_room.save
+            @new_room.broadcast_append_to :rooms
+        end
+    end
+
+    def show
+        @room.find_by!(title: params[:title])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   get "/account", to: "users#account"
   get "/account/settings", to: "users#settings"
 
+  get "/rooms", to: "rooms#new"
+
   resources :licenses
   resource :session, only: %i[new create destroy]
+  resources :rooms, only: %i[create show new], param: :title
   resources :users
   resources :departments
 
